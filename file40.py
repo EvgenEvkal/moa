d81d906e2c2fa9b9ee990129a70fd9f01b3076f9
EvgenEvkal <zenavershinin2008@gmail.com>
2025-10-17 00:35:33 +0300

    1

--- file40.py
+++ file40.py
@@ -1,2 +1,12 @@
 #Begin40 Вершинин ИСП 111
-A1 = float()
+A1 = float(input())
+B1 = float(input())
+C1 = float(input())
+A2 = float(input())
+B2 = float(input())
+C2 = float(input())
+D = A1 * B2 - A2 * B1
+x = (C1 * B2 - C2 * B1) / D
+y = (A1 * C2 - A2 * C1) / D
+print(x)
+print(y)
