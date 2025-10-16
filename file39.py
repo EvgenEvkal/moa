f9d39a168b8e76125438e1714c2a9466824ad2c5
EvgenEvkal <zenavershinin2008@gmail.com>
2025-10-17 00:13:23 +0300

    1

--- file39.py
+++ file39.py
@@ -4,15 +4,15 @@ B = float(input())
 C = float(input())
 if a != 0:
     D = B ** 2 - 4 * A * C
-        if D > 0:
-                 x1 = (-b - D ** 0,5) / (2  * a)
-                          x2 = (-b + D ** 0,5) / (2 * a)
-                                   print(x1)
-                                            print(x2)
-                                                elif D == 0;
-                                                         x1 = (-b - D ** 0,5) / (2 * a)
-                                                                  print(x1)
-                                                                      else:
-                                                                               print("Корней нету!")
-                                                                               else:
-                                                                                    print("Коэффициент А не должен быть равен 0!")
+    if D > 0:
+        x1 = (-b - D ** 0,5) / (2  * a)
+        x2 = (-b + D ** 0,5) / (2 * a)
+    print(x1)
+    print(x2)
+    elif D == 0:
+        x1 = (-b - D ** 0,5) / (2 * a)
+        print(x1)
+    else:
+        print("Корней нету!")
+else:
+    print("Коэффициент А не должен быть равен 0!")
