6e65aa6dd4f17608916ecee1b44220556c6c022d
EvgenEvkal <zenavershinin2008@gmail.com>
2025-10-16 23:43:22 +0300

    update

--- file37.py
+++ file37.py
@@ -1,10 +1,10 @@
-#Begin36 Вершинин ИСП 111
+#Begin37 Вершинин ИСП 111
 V1 = float(input())
 V2 = float(input())
 S = float(input())
 T = float(input())
 if V1 >= 0 and V2 >= 0 and S >= 0 and T >= 0:
-    S += V1 * T + V2 * T
-    print(S)
+    S -= V1 * T + V2 * T
+    print(abs(S))
 else:
     print("Один из аргументов отрицательный!")
