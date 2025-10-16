266738326f3e13820b9e8b4f51a7653cb4cb4307
EvgenEvkal <zenavershinin2008@gmail.com>
2025-10-16 23:29:45 +0300

    redact

--- file34.py
+++ file34.py
@@ -1,1 +1,10 @@
 #Begin34 Вершинин ИСП 111
+X = float(input())
+A = float(input())
+Y = float(input())
+B = float(input())
+one_kg_ch = A/X
+one_kg_ir = B/Y
+print(one_kg_ch)
+print(one_kg_ir)
+print(one_kg_ch/one_kg_ir)
